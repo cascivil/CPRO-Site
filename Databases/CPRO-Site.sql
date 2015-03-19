f1e548337c0c4980cbbd3a61a2f3ba7dedb13290
Felipe Souza <fasfelipe@gmail.com>
2015-03-19 00:09:47 -0300

    Ajustes na base de dados
    Script do banco de dados em PostgreSQL ajustado e pronto para execução.

--- Databases/CPRO-Site.sql
+++ Databases/CPRO-Site.sql
@@ -1,6 +1,5 @@
-/*
-* Database Creation
-*/
+
+------------------------------------------ Create and Drop Database ------------------------------------------
 
 DROP DATABASE IF EXISTS "CProSite";
 
@@ -10,28 +9,49 @@ CREATE DATABASE "CProSite"
 	LC_COLLATE = 'Portuguese_Brazil.1252'
 	LC_CTYPE = 'Portuguese_Brazil.1252'
 	CONNECTION LIMIT = -1;
+--------------------------------------------------------------------------------------------------------------
+
+
+----------------------------------------------- Database Set -------------------------------------------------
+\c CProSite
+--------------------------------------------------------------------------------------------------------------
+
+
+------------------------------------------ Create and Drop Schemas -------------------------------------------
 
 DROP SCHEMA IF EXISTS Configuracao CASCADE;
+DROP SCHEMA IF EXISTS RedeSocial CASCADE;
 DROP SCHEMA IF EXISTS Publicacao CASCADE;
 DROP SCHEMA IF EXISTS Cliente CASCADE;
 DROP SCHEMA IF EXISTS Email CASCADE;
 DROP SCHEMA IF EXISTS Log CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS Configuracao;
+CREATE SCHEMA IF NOT EXISTS RedeSocial;
 CREATE SCHEMA IF NOT EXISTS Publicacao;
 CREATE SCHEMA IF NOT EXISTS Cliente;
 CREATE SCHEMA IF NOT EXISTS Email;
 CREATE SCHEMA IF NOT EXISTS Log;
+--------------------------------------------------------------------------------------------------------------
 
+
+---------------------------------------- Create Database Types (ENUM) ----------------------------------------
+
+CREATE TYPE GENERO AS ENUM ('Indefinido', 'Masculino', 'Feminino');
+CREATE TYPE TELEFONE AS ENUM ('Indefinido', 'Residencial', 'Comercial', 'FAX', 'Celular', 'Outro');
+--------------------------------------------------------------------------------------------------------------
+
+
+-------------------------------------------- Create Databases UML --------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Configuracao.Sistema
 (
 	Codigo SERIAL NOT NULL,
 	Nome VARCHAR(200) NOT NULL,
-	Ativo BIT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	CONSTRAINT PK_Codigo PRIMARY KEY (Codigo)
+	PRIMARY KEY (Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Configuracao.Campanha
@@ -39,13 +59,13 @@ CREATE TABLE IF NOT EXISTS Configuracao.Campanha
 	Codigo SERIAL NOT NULL,
 	CodigoSistema INT NOT NULL,
 	Nome VARCHAR(200) NOT NULL,
-	Ativo BIT NOT NULL,
-	DataInicio TIMESTAMP NOT NULL,
-	DataTermino TIMESTAMP NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	DataInicio TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataTermino TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoSistema" FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Configuracao.SubCampanha
@@ -53,13 +73,13 @@ CREATE TABLE IF NOT EXISTS Configuracao.SubCampanha
 	Codigo SERIAL NOT NULL,
 	CodigoCampanha INT NOT NULL,
 	Nome VARCHAR(200) NOT NULL,
-	Ativo BIT NOT NULL,
-	DataInicio TIMESTAMP NOT NULL,
-	DataTermino TIMESTAMP NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	DataInicio TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataTermino TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCampanha" FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Configuracao.Procedure
@@ -67,42 +87,12 @@ CREATE TABLE IF NOT EXISTS Configuracao.Procedure
 	Codigo SERIAL NOT NULL,
 	CodigoSistema INT NOT NULL,
 	Nome VARCHAR(200) NOT NULL,
-	Ativo BIT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_" FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
 );
-
-CREATE TABLE IF NOT EXISTS Log.LogSistema
-(
-	Codigo SERIAL NOT NULL,
-	CodigoUsuario INT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
-
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
-);
-
-CREATE TABLE IF NOT EXISTS Log.LogAuditoria
-(
-	Codigo SERIAL NOT NULL,
-	CodigoSistema INT NOT NULL,
-	CodigoProcedure INT NOT NULL,
-	CodigoUsuario INT,
-	DataCriacao TIMESTAMP NOT NULL,
-	IP VARCHAR(30),
-	Parametros TEXT(2000),
-	Resultado TINYINT,
-	MensagemErro TEXT(2000),
-
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoSistema" FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo),
-	CONSTRAINT "FK_CodigoProcedure" FOREIGN KEY (CodigoProcedure) REFERENCES Configuracao.Procedure(Codigo),
-	CONSTRAINT "FK_CodigoUsuario" FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo)
-);
-
-
-
 
 CREATE TABLE IF NOT EXISTS Cliente.Usuario
 (
@@ -113,29 +103,43 @@ CREATE TABLE IF NOT EXISTS Cliente.Usuario
 	Senha VARCHAR(255) NOT NULL,
 	ChaveTrocaSenha VARCHAR(255),
 	ChaveAtivacao VARCHAR(100),
-	TentativasInvalidas TINYINT SIGNED,
-	Bloqueio BIT NOT NULL,
-	Ativo BIT NOT NULL,
-	Debug BIT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	TentativasInvalidas SMALLINT,
+	Bloqueio BOOLEAN NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	Debug BOOLEAN NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	DataAlteracao TIMESTAMP,
 	DataUltimoAcesso TIMESTAMP,
 	DataTrocaSenha TIMESTAMP,
 	DataLiberacaoBloqueio TIMESTAMP,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCliente" FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
-	CONSTRAINT "FK_CodigoSistema" FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
+	PRIMARY KEY (Codigo),
+	--FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Cliente.HistoricoAcesso
 (
 	CodigoUsuario INT,
-	DataCriacao TIMESTAMP NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	IP VARCHAR(30),
 	Login VARCHAR,
 
-	CONSTRAINT "FK_CodigoUsuario" FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo)
+	FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Cliente.Telefone
+(
+	Codigo SERIAL NOT NULL,
+	Telefone VARCHAR(15) NOT NULL,
+	TelefoneDDD VARCHAR(8) NOT NULL,
+	TipoTelefone TELEFONE NOT NULL, 
+	OptinWhastApp BOOLEAN NOT NULL,
+	OptinSMS BOOLEAN NOT NULL,
+	Ativo BOOLEAN NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Cliente.Cliente
@@ -150,7 +154,7 @@ CREATE TABLE IF NOT EXISTS Cliente.Cliente
 	Documento VARCHAR(50),
 	Email VARCHAR(200),
 	Nascimento DATE,
-	CodigoGenero TINYINT,
+	Genero GENERO,
 
 	Cidade VARCHAR(255),
 	Estado VARCHAR(100),
@@ -160,115 +164,160 @@ CREATE TABLE IF NOT EXISTS Cliente.Cliente
 	EnderecoComplemento VARCHAR(200),
 	EnderecoBairro VARCHAR(255),
 
-	DataCriacao TIMESTAMP NOT NULL,
-	DataAlteracao TIMESTAMP NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataAlteracao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoUsuario" FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo),
-	CONSTRAINT "FK_CodigoSistema" FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo),
-	CONSTRAINT "FK_CodigoTelefone" FOREIGN KEY (CodigoTelefone) REFERENCES Cliente.Telefone(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo),
+	FOREIGN KEY (CodigoTelefone) REFERENCES Cliente.Telefone(Codigo)
 );
 
+
+CREATE TABLE IF NOT EXISTS Configuracao.Modulo
+(
+	Codigo SERIAL NOT NULL,
+	CodigoSistema INT NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Descricao VARCHAR(200),
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
+);
 
 CREATE TABLE IF NOT EXISTS Configuracao.Perfil
 (
 	Codigo SERIAL NOT NULL,
+	CodigoSistema INT NOT NULL,
 	Nome VARCHAR(100) NOT NULL,
-	Descricao VARCHAR(255),
-	Ordem SMALLINT,
-	DataCriacao TIMESTAMP NOT NULL,
+	Descricao VARCHAR(200),
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo)
 );
 
+CREATE TABLE IF NOT EXISTS Configuracao.ModuloPerfil
+(
+	CodigoSistema INT NOT NULL,
+	CodigoPerfil INT NOT NULL,
+	CodigoModulo INT NOT NULL,
+	OrdemExibicao SMALLINT,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (CodigoSistema, CodigoPerfil, CodigoModulo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo),
+	FOREIGN KEY (CodigoPerfil) REFERENCES Configuracao.Perfil(Codigo),
+	FOREIGN KEY (CodigoModulo) REFERENCES Configuracao.Modulo(Codigo)
+);
 
 CREATE TABLE IF NOT EXISTS Configuracao.ClientePerfil
 (
 	CodigoUsuario INT NOT NULL,
 	CodigoPerfil INT NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	CONSTRAINT "PK_Usuario_Perfil" PRIMARY KEY (CodigoUsuario, CodigoPerfil),
-	CONSTRAINT "FK_CodigoUsuario" FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo),
-	CONSTRAINT "FK_CodigoPerfil" FOREIGN KEY (CodigoPerfil) REFERENCES Configuracao.Perfil(Codigo)
+	PRIMARY KEY (CodigoUsuario, CodigoPerfil),
+	FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo),
+	FOREIGN KEY (CodigoPerfil) REFERENCES Configuracao.Perfil(Codigo)
 );
 
-CREATE TABLE IF NOT EXISTS Cliente.TipoRedeSocial
+CREATE TABLE IF NOT EXISTS RedeSocial.Facebook
 (
 	Codigo SERIAL NOT NULL,
-	Nome VARCHAR(50) NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	CodigoCliente INT NOT NULL, 
+	TokenAcesso VARCHAR(250) NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
+	PRIMARY KEY (Codigo),
+	UNIQUE (Codigo, CodigoCliente),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS RedeSocial.LinkedIn
+(
+	Codigo SERIAL NOT NULL,
+	CodigoCliente INT NOT NULL, 
+	TokenAcesso VARCHAR(250) NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo),
+	UNIQUE (Codigo, CodigoCliente),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS RedeSocial.Twitter
+(
+	Codigo SERIAL NOT NULL,
+	CodigoCliente INT NOT NULL, 
+	TokenAcesso VARCHAR(250) NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo),
+	UNIQUE (Codigo, CodigoCliente),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS RedeSocial.Instagram
+(
+	Codigo SERIAL NOT NULL,
+	CodigoCliente INT NOT NULL, 
+	TokenAcesso VARCHAR(250) NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo),
+	UNIQUE (Codigo, CodigoCliente),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Cliente.RedeSocial
 (
 	Codigo SERIAL NOT NULL,
 	CodigoCliente INT NOT NULL,
-	CodigoRedeSocial
+	CodigoFacebook INT,
+	CodigoInstagram INT,
+	CodigoLinkedin INT,
+	CodigoTwitter INT,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCliente" FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
-	CONSTRAINT "FK_CodigoRedeSocial" FOREIGN KEY (CodigoRedeSocial) REFERENCES Cliente.TipoRedeSocial(Codigo)
+	PRIMARY KEY (Codigo, CodigoCliente),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
+	FOREIGN KEY (CodigoFacebook) REFERENCES RedeSocial.Facebook(Codigo),
+	FOREIGN KEY (CodigoInstagram) REFERENCES RedeSocial.Instagram(Codigo),
+	FOREIGN KEY (CodigoLinkedin) REFERENCES RedeSocial.LinkedIn(Codigo),
+	FOREIGN KEY (CodigoTwitter) REFERENCES RedeSocial.Twitter(Codigo)
 );
 
-
-CREATE TABLE IF NOT EXISTS Cliente.Telefone
-(
-	Codigo SERIAL NOT NULL,
-	Telefone VARCHAR(15) NOT NULL,
-	TelefoneDDD VARCHAR(8) NOT NULL,
-	CodigoWhastAppStatus SMALLINT(6) NOT NULL,
-
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoWhastAppStatus" FOREIGN KEY (CodigoWhastAppStatus) REFERENCES Cliente.WhastAppStatus(Codigo)
-);
-
-
-CREATE TABLE IF NOT EXISTS Cliente.WhastAppStatus
-(
-	Codigo SERIAL NOT NULL,
-	Descricao VARCHAR(100) NOT NULL,
-
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
-);
 
 CREATE TABLE IF NOT EXISTS Cliente.TipoOptin
-
+(
 	Codigo SMALLSERIAL NOT NULL,
 	Descricao VARCHAR(50) NOT NULL,
-	Ativo BIT NOT NULL,
+	Ativo BOOLEAN NOT NULL,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
+	PRIMARY KEY (Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Cliente.ClienteOptin
 (
 	CodigoCliente INT NOT NULL,
 	CodigoTipoOptin INT NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	CONSTRAINT "PK_Codigo" PRIMARY KEY (CodigoCliente, CodigoTipoOptin),
-	CONSTRAINT "FK_CodigoCliente" FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
-	CONSTRAINT "FK_CodigoTipoOptin" FOREIGN KEY (CodigoTipoOptin) REFERENCES Cliente.TipoOptin(Codigo)
+	PRIMARY KEY (CodigoCliente, CodigoTipoOptin),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
+	FOREIGN KEY (CodigoTipoOptin) REFERENCES Cliente.TipoOptin(Codigo)
 );
-
-CREATE TABLE IF NOT EXISTS Cliente.Genero
-(
-	Codigo SMALLSERIAL NOT NULL,
-	Descricao VARCHAR(30) NOT NULL,
-
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
-);
-
 
 -- Cria o disparo de determinado e-mail sistêmico para determinado cliente
 CREATE TABLE IF NOT EXISTS Email.DisparoEmail
 (
 	Codigo SERIAL NOT NULL,
 	CodigoCliente INT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	CONSTRAINT "FK_CodigoCliente" FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo)
 );
 
 -- Armazena os templates de e-mails sistêmicos associado a entidade Campanha
@@ -276,14 +325,14 @@ CREATE TABLE IF NOT EXISTS Email.TemplateEmail
 (
 	Codigo SERIAL NOT NULL,
 	CodigoCampanha INT NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	Descricao VARCHAR(200) NOT NULL,
 	Titulo VARCHAR(200) NOT NULL,
-	HTML TEXT(65000) NOT NULL,
-	Ativo BIT NOT NULL,
+	HTML TEXT NOT NULL,
+	Ativo BOOLEAN NOT NULL,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCampanha" FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
 );
 
 -- Armazena os parâmetros dinâmicos dos e-mails sistêmicos
@@ -291,7 +340,7 @@ CREATE TABLE IF NOT EXISTS Email.ParametroEmail
 (
 	CodigoTemplateEmail INT NOT NULL,
 	NomeParametro VARCHAR(100) NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp
 
 );
 
@@ -318,18 +367,18 @@ CREATE TABLE IF NOT EXISTS Publicacao.Publicacao
 	CodigoCampanha INT NOT NULL,
 	Nome VARCHAR(50),
 	Descricao VARCHAR(200),
-	DataCriacao TIMESTAMP NOT NULL,
-	DataAlteracao TIMESTAMP,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	DataAlteracao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	CodigoImagem INT,
 	LinkImagem VARCHAR(200),
 	TituloArtigo VARCHAR(200),
-	Artigo TEXT(65000),
-	Resumo TEXT(30000),
-	Publicado BIT NOT NULL,
-	Ativo BIT NOT NULL,
+	Artigo TEXT,
+	Resumo TEXT,
+	Publicado BOOLEAN NOT NULL,
+	Ativo BOOLEAN NOT NULL,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCampanha" FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoCampanha) REFERENCES Configuracao.Campanha(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Publicacao.AutorPublicacao
@@ -339,9 +388,9 @@ CREATE TABLE IF NOT EXISTS Publicacao.AutorPublicacao
 	CodigoPublicacao INT NOT NULL,
 
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo),
-	CONSTRAINT "FK_CodigoCliente" FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
-	CONSTRAINT "FK_CodigoPublicacao" FOREIGN KEY (CodigoPublicacao) REFERENCES Publicacao.Publicacao(Codigo)
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoCliente) REFERENCES Cliente.Cliente(Codigo),
+	FOREIGN KEY (CodigoPublicacao) REFERENCES Publicacao.Publicacao(Codigo)
 );
 
 CREATE TABLE IF NOT EXISTS Publicacao.Imagem
@@ -350,17 +399,50 @@ CREATE TABLE IF NOT EXISTS Publicacao.Imagem
 	Nome VARCHAR(50),
 	Descricao VARCHAR(200),
 	Imagem OID NOT NULL,
-	DataCriacao TIMESTAMP NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
 
-	--CONSTRAINT "PK_Codigo" PRIMARY KEY (Codigo)
+	PRIMARY KEY (Codigo)
 );
 
+
+CREATE TABLE IF NOT EXISTS Log.LogSistema
+(
+	Codigo SERIAL NOT NULL,
+	CodigoUsuario INT NOT NULL,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+	PRIMARY KEY (Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Log.LogAuditoria
+(
+	Codigo SERIAL NOT NULL,
+	CodigoSistema INT NOT NULL,
+	CodigoProcedure INT NOT NULL,
+	CodigoUsuario INT,
+	DataCriacao TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	IP VARCHAR(30),
+	Parametros TEXT,
+	Resultado SMALLINT,
+	MensagemErro TEXT,
+
+	PRIMARY KEY (Codigo),
+	FOREIGN KEY (CodigoSistema) REFERENCES Configuracao.Sistema(Codigo),
+	FOREIGN KEY (CodigoProcedure) REFERENCES Configuracao.Procedure(Codigo),
+	FOREIGN KEY (CodigoUsuario) REFERENCES Cliente.Usuario(Codigo)
+);
+--------------------------------------------------------------------------------------------------------------
+
+
+-------------------------------------------- Insert Principal Data -------------------------------------------
+
 -- Inserção de Sistema padrão de gerenciamento
-INSERT INTO Configuracao.Sistema(Nome, Ativo) VALUES ("Gerenciamento Banco de Dados", 1);
+INSERT INTO Configuracao.Sistema (Nome, Ativo) VALUES ('Gerenciamento Banco de Dados', TRUE);
+--------------------------------------------------------------------------------------------------------------
 
--- Inserção de tipos de redes sociais
-INSERT INTO Cliente.TipoRedeSocial(Nome) VALUES ("Facebook"), ("Twitter"), ("Instagram"), ("LinkedIn");
 
--- Libera permissão para o Root e usrAppCPRO
-GRANT ALL PRIVILEGES ON CProSite.* TO postgres@localhost IDENTIFIED BY "recall00";
-GRANT SELECT, INSERT ON CProSite.* TO usrAppCPro@localhost IDENTIFIED BY "Sj6eA76Hf2Y4f56xG6BW";
+-------------------------------- Grant Access To Users on Schemas and Tables ---------------------------------
+
+GRANT ALL PRIVILEGES ON "CProSite" TO postgres;
+GRANT SELECT, INSERT ON "CProSite" TO usrAppCPro;
+--------------------------------------------------------------------------------------------------------------
